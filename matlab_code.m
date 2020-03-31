a94504b8e5ec297aea2bd26f06c47e8cf5978406
Belal Hmedan <55383642+bhomaidan1990@users.noreply.github.com>
2020-03-31 10:29:12 +0200

    Update Matlab code
    Added show detected points feature

--- matlab_code.m
+++ matlab_code.m
@@ -10,17 +10,20 @@ warning('off','all')
 %===========================
 % 1-Prepare Calibration Images
 %===========================
+% Initialization
 path = 'Belal_checkboard/image_%03d.jpg'; % modify according to your path and images name and extension
 numImages = 20; % modify according to number of images you want to read
+magnification = 26; % pixel per mm
+squareSize = 26; % in millimeters
 
-files = cell(1, numImages);
+file_names = cell(1, numImages);
 for n=1:numImages
-  files{n} = sprintf(path,n);
+  file_names{n} = sprintf(path,n);
 end
 %imshow(images(1,1))
 % Display one of the calibration images
-magnification = 26;
-I = imread(files{1});
+
+I = imread(file_names{1});
 figure('Name','One of the Calibration Images','NumberTitle','off');
 imshow(I, 'InitialMagnification', magnification);
 title('One of the Calibration Images');
@@ -30,11 +33,24 @@ title('One of the Calibration Images');
 % 2-Estimate Camera Parameters
 %========================
 % Detect the checkerboard corners in the images.
-[imagePoints, boardSize] = detectCheckerboardPoints(files);
-
+[imagePoints, boardSize, imagesUsed] = detectCheckerboardPoints(file_names);
+% Plot the detected points
+%---------------------------
+file_names2 = file_names(imagesUsed);
+figure('Name','detected Points','NumberTitle','off');
+%for i = 1:numel(file_names2) % Uncomment_1
+for i = 1:4 % Comment_1
+  I = imread(file_names2{i});
+  %subplot(5, 4, i); % Uncomment_2
+  subplot(2, 2, i); % Comment_2
+  imshow(I);
+  title(['image',num2str(i)])
+  hold on;
+  plot(imagePoints(:,1,i),imagePoints(:,2,i),'ro');
+end
+%---------------------------
 % Generate the world coordinates of the checkerboard corners in the
 % pattern-centric coordinate system, with the upper-left corner at (0,0).
-squareSize = 26*26; % in millimeters
 worldPoints = generateCheckerboardPoints(boardSize, squareSize);
 
 % Calibrate the camera.
@@ -50,7 +66,7 @@ title('Reprojection Errors');
 %========================
 % 3-Read the Image of Objects to Be Measured
 %========================
-imOrig = imread(files{9});
+imOrig = imread(file_names{9});
 figure('Name','Input Image','NumberTitle','off');
 imshow(imOrig, 'InitialMagnification', magnification);
 title('Input Image');
